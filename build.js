f7d4dc237a014db0079f3ceab3f3e86340b631c3
scrollbar-ww <scrollbar@163.com>
2018-08-07 18:50:34 +0800

    + stylelint

--- build.js
+++ build.js
@@ -1,32 +1,32 @@
 // postcss & plugins
-const autoPrefixer = require('autoprefixer')
-const atImport = require('postcss-import')
+const atImport = require('postcss-import')()
 const mixins = require('postcss-mixins')
+const vars = require('postcss-simple-vars')
 const nested = require('postcss-nested')
+const colorMix = require('postcss-color-mix')
 const unPrefix = require('postcss-unprefix')
-const postcss = require('postcss')([atImport(), mixins, nested, unPrefix, autoPrefixer])
+const autoPrefixer = require('autoprefixer')
+const postcss = require('postcss')([atImport, mixins, vars, nested, colorMix, unPrefix, autoPrefixer])
 
 // formater
 const CleanCss = require('clean-css')
 const cleaner = new CleanCss({
   format: {
     breaks: {
-      // controls where to insert breaks
-      afterAtRule: true, // controls if a line break comes after an at-rule; e.g. `@charset`; defaults to `false`
-      afterBlockBegins: true, // controls if a line break comes after a block begins; e.g. `@media`; defaults to `false`
-      afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
-      afterComment: true, // controls if a line break comes after a comment; defaults to `false`
-      afterProperty: true, // controls if a line break comes after a property; defaults to `false`
-      afterRuleBegins: true, // controls if a line break comes after a rule begins; defaults to `false`
-      afterRuleEnds: true, // controls if a line break comes after a rule ends; defaults to `false`
-      beforeBlockEnds: true, // controls if a line break comes before a block ends; defaults to `false`
-      betweenSelectors: true // controls if a line break comes between selectors; defaults to `false`
+      afterAtRule: true,
+      afterBlockBegins: true,
+      afterBlockEnds: true,
+      afterComment: true,
+      afterProperty: true,
+      afterRuleBegins: true,
+      afterRuleEnds: true,
+      beforeBlockEnds: true,
+      betweenSelectors: true
     },
     spaces: {
-      // controls where to insert spaces
-      aroundSelectorRelation: true, // controls if spaces come around selector relations; e.g. `div > a`; defaults to `false`
-      beforeBlockBegins: true, // controls if a space comes before a block begins; e.g. `.block {`; defaults to `false`
-      beforeValue: true // controls if a space comes before a value; e.g. `width: 1rem`; defaults to `false`
+      aroundSelectorRelation: true,
+      beforeBlockBegins: true,
+      beforeValue: true
     },
     indentBy: 2
   }
@@ -38,10 +38,10 @@ fs.readFile('src/index.pcss', (err, css) => {
     console.error(err)
   } else {
     postcss
-      .process(css, { from: 'src/index.pcss', to: 'dist/sparrow.css' })
+      .process(css, { from: 'src/index.pcss' })
       .then(({ css }) => {
         fs.writeFile('dist/sparrow.css', cleaner.minify(css).styles, err => {
-          err ? console.error(err) : console.info('[done]', 'dist/sparrow.css')
+          err ? console.error(err) : console.info('done', '=>', 'dist/sparrow.css')
         })
       })
       .catch(err => console.error(err))
