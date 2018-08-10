a8d8e6a517849d98dc326115ce750c7d0c1b3eb6
scrollbar-ww <scrollbar@163.com>
2018-08-10 15:19:01 +0800

    + watch

--- build.js
+++ build.js
@@ -1,12 +1,24 @@
 // postcss & plugins
 const atImport = require('postcss-import')()
-const mixins = require('postcss-mixins')
+const atMixins = require('postcss-mixins')
 const vars = require('postcss-simple-vars')
 const nested = require('postcss-nested')
 const colorMix = require('postcss-color-mix')
+const atIf = require('postcss-conditionals')
+const atFor = require('postcss-for')
 const unPrefix = require('postcss-unprefix')
 const autoPrefixer = require('autoprefixer')
-const postcss = require('postcss')([atImport, mixins, vars, nested, colorMix, unPrefix, autoPrefixer])
+const postcss = require('postcss')([
+  atImport,
+  atMixins,
+  vars,
+  nested,
+  colorMix,
+  atIf,
+  atFor,
+  unPrefix,
+  autoPrefixer
+])
 
 // formater
 const CleanCss = require('clean-css')
@@ -33,17 +45,43 @@ const cleaner = new CleanCss({
 })
 
 const fs = require('fs')
-fs.readFile('src/index.pcss', (err, css) => {
-  if (err) {
-    console.error(err)
-  } else {
-    postcss
-      .process(css, { from: 'src/index.pcss' })
-      .then(({ css }) => {
-        fs.writeFile('dist/sparrow.css', cleaner.minify(css).styles, err => {
-          err ? console.error(err) : console.info('done', '=>', 'dist/sparrow.css')
+const { resolve } = require('path')
+const srcRoot = resolve(__dirname, 'src')
+const srcEntry = resolve(srcRoot, 'index.pcss')
+const distFile = resolve(__dirname, 'dist', 'sparrow.css')
+
+let processing = false
+function bundle () {
+  processing = true
+  fs.readFile(srcEntry, (err, css) => {
+    if (err) {
+      console.error(err)
+    } else {
+      postcss
+        .process(css, { from: srcEntry })
+        .then(({ css }) => {
+          fs.writeFile(distFile, cleaner.minify(css).styles, err => {
+            err ? console.error(err) : console.info('done', '=>', 'dist/sparrow.css')
+            processing = false
+          })
         })
-      })
-      .catch(err => console.error(err))
-  }
-})
+        .catch(err => {
+          console.error(err)
+          processing = false
+        })
+    }
+  })
+}
+bundle()
+
+if (require('yargs').alias('w', 'watch').argv.watch) {
+  console.info('watching on ...', 'press ctrl + c to exit.')
+  require('chokidar')
+    .watch(srcRoot, {
+      ignoreInitial: true,
+      ignored: /(^|[/\\])\../
+    })
+    .on('all', event => {
+      if ((event === 'change' || event === 'add' || event === 'unlink') && !processing) bundle()
+    })
+}
