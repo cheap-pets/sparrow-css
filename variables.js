c4282ec71311d2042483d081541e2b0b87f9fb8e
scrollbar <scrollbar@163.com>
2019-02-19 17:08:32 +0800

    rewrite button styles

--- variables.js
+++ variables.js
@@ -2,15 +2,16 @@ const preset = require('@ant-design/colors').presetPalettes
 
 const colors = {
   PRIMARY: preset.cyan,
-  SECONDARY: preset.gold,
-  DANGER: preset.red,
+  SECONDARY: preset.magenta,
+  DANGER: preset.volcano,
   SUCCESS: preset.green,
   WARNING: preset.orange,
   GREY: [ '#ffffff', '#fafafa', '#f5f5f5', '#e8e8e8', '#d9d9d9', '#bfbfbf', '#8c8c8c', '#595959', '#262626', '#000000' ]
 }
 
 const variables = {
-  INPUT_BORDER_RADIUS: 4
+  INPUT_BORDER_RADIUS: 4,
+  SHOW_BUTTON_SHADOW: 1
 }
 
 Object.keys(colors).forEach(key => {
