c73332062b872bb98323c3f970321825a3211457
scrollbar <scrollbar@163.com>
2019-06-20 18:56:19 +0800

    modify colors

--- variables.js
+++ variables.js
@@ -1,4 +1,16 @@
-const { blue, grey, green, orange, volcano } = require('@ant-design/colors').presetPalettes
+const { blue, green, orange, yellow, volcano } = require('@ant-design/colors').presetPalettes
+const grey = [
+  '#f2f2f2',
+  '#e6e6e6',
+  '#d9d9d9',
+  '#cccccc',
+  '#bfbfbf',
+  '#b2b2b2',
+  '#8c8c8c',
+  '#666666',
+  '#404040',
+  '#1a1a1a'
+]
 
 // const colors = {
 //   PRIMARY: preset.blue,
@@ -13,37 +25,62 @@ const variables = {
   UNIT_SIZE: 8,
 
   COLOR_PRIMARY: blue[5],
-  COLOR_PRIMARY_HOVER: blue[4],
-  COLOR_PRIMARY_ACTIVE: blue[6],
-  COLOR_PRIMARY_DISABLED: blue[2],
-
-  COLOR_SECONDARY: grey[5],
-  COLOR_SECONDARY_HOVER: grey[4],
-  COLOR_SECONDARY_ACTIVE: grey[6],
-  COLOR_SECONDARY_DISABLED: grey[2],
-
+  COLOR_SECONDARY: grey[7],
   COLOR_SUCCESS: green[5],
-  COLOR_SUCCESS_HOVER: green[4],
-  COLOR_SUCCESS_ACTIVE: green[6],
-  COLOR_SUCCESS_DISABLED: green[2],
-
   COLOR_DANGER: volcano[5],
-  COLOR_DANGER_HOVER: volcano[4],
-  COLOR_DANGER_ACTIVE: volcano[6],
-  COLOR_DANGER_DISABLED: volcano[2],
-
-  COLOR_GREY: grey[4],
-  COLOR_GREY_HOVER: grey[3],
-  COLOR_GREY_ACTIVE: grey[5],
-  COLOR_GREY_DISABLED: grey[0],
-
   COLOR_WARNING: orange[5],
+
+  SHADOW_COLOR_NORMAL: grey[4],
+  SHADOW_COLOR_PRIMARY: blue[2],
+  SHADOW_COLOR_SECONDARY: grey[4],
+  SHADOW_COLOR_SUCCESS: green[2],
+  SHADOW_COLOR_DANGER: volcano[2],
+  
+  BUTTON_COLOR_NORMAL: grey[7],
+  BUTTON_COLOR_NORMAL_HOVER: grey[6],
+  BUTTON_COLOR_NORMAL_ACTIVE: grey[8],
+  BUTTON_COLOR_NORMAL_DISABLED: grey[4],
+
+  BUTTON_COLOR_PRIMARY: blue[5],
+  BUTTON_COLOR_PRIMARY_HOVER: blue[4],
+  BUTTON_COLOR_PRIMARY_ACTIVE: blue[6],
+  BUTTON_COLOR_PRIMARY_DISABLED: blue[2],
+  
+  BUTTON_COLOR_SECONDARY: grey[7],
+  BUTTON_COLOR_SECONDARY_HOVER: grey[6],
+  BUTTON_COLOR_SECONDARY_ACTIVE: grey[8],
+  BUTTON_COLOR_SECONDARY_DISABLED: grey[4],
+
+  BUTTON_COLOR_SUBMIT: green[5],
+  BUTTON_COLOR_SUBMIT_HOVER: green[4],
+  BUTTON_COLOR_SUBMIT_ACTIVE: green[6],
+  BUTTON_COLOR_SUBMIT_DISABLED: green[2],
+
+  BUTTON_COLOR_DANGER: volcano[5],
+  BUTTON_COLOR_DANGER_HOVER: volcano[4],
+  BUTTON_COLOR_DANGER_ACTIVE: volcano[6],
+  BUTTON_COLOR_DANGER_DISABLED: volcano[2],
+
+  BUTTON_SHADOW: 1,
+    
+  INPUT_BORDER_COLOR: grey[7],
+  INPUT_COLOR_READONLY: yellow[0],
+  INPUT_COLOR_DISABLED: grey[1],
+
+  INPUT_BORDER_RADIUS: 2,
+  INPUT_HEIGHT: 32,
 
   BAR_HEIGHT: 40,
 
-  BUTTON_SHADOW: 1,
+  BORDER_COLOR_DEFAULT: grey[7],
 
-  DEFAULT_TEXT_COLOR: '#333',
+  TEXT_COLOR_DEFAULT: grey[9],
+  TEXT_COLOR_DARK_GREY: grey[8],
+  TEXT_COLOR_GREY: grey[7],
+  TEXT_COLOR_LIGHT_GREY: grey[6],
+
+  TEXT_COLOR_LIGHT: 'rgba(255, 255, 255, .75)',
+  TEXT_COLOR_DARK: 'rgba(0, 0, 0, .75)',
 
   DIALOG_BORDER_RADIUS: 0,
 
@@ -51,8 +88,6 @@ const variables = {
 
   FORM_LABEL_WIDTH: 65,
 
-  INPUT_BORDER_RADIUS: 2,
-  INPUT_HEIGHT: 32,
 
   LIST_ITEM_HEIGHT: 40
 }
